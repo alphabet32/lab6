4f66e1d1bd86faeb3c8136a6a1b9a6a24b659aaf
Nicholas Siphengphone <nsipheng@ucsd.edu>
2016-02-11 18:44:39 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -12,6 +12,7 @@ function initializePage() {
 	$('.project a').click(addProjectDetails);
 
 	$('#colorBtn').click(randomizeColors);
+
 }
 
 /*
@@ -27,6 +28,8 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+	console.log("/project/" + idNumber);
+	$.get("/project/" + idNumber, callbackFn);
 }
 
 /*
@@ -35,4 +38,29 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette", callbackFn2);
 }
+
+// callback function
+function callbackFn(result) {
+	// this is the callback function
+	console.log(result);
+	var projectHTML =
+		'<img src="' + result['image'] + '"class=detailsImage">' +
+		'<p><small>' + result['date'] + '</small></p>' +
+		'<p><small>' + result['summary'] + '</small></p></a>'; 
+	$("#project" + result['id'] + " .details").html(projectHTML);
+
+}
+
+function callbackFn2(result) {
+	console.log(result);
+	var colors = (result['colors'])['hex'];
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
+
+}
+
